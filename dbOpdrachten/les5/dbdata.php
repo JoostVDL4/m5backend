e0d08e5ad6cd21fec735fb6da59829cfc1bb0555
Joost <35542@ma-web.nl>
2023-10-19 10:22:59 +0200

    les5

---
+++ dbOpdrachten/les5/dbdata.php
@@ -0,0 +1,14 @@
+<?php
+
+$databaseName = $envSettings["DB_NAME"];
+$dbUser =  $envSettings["DB_USER"];
+$dbPassword =  $envSettings["DB_PASSWORD"];
+$dbHost = $envSettings["DB_HOST"];
+
+
+$envSettings = [];
+    if (file_exists(dirname(__FILE__).'/.env')) {
+        $envSettings = parse_ini_file(dirname(__FILE__).'/.env');
+    }
+// als file bestaat, dan wordt envSettings een ini file?
+?>
---
+++ dbOpdrachten/les5/test.php
@@ -0,0 +1,15 @@
+<?php
+
+include "dbdata.php";
+
+function ConnectToDatabase($databaseName, $dbUser, $dbPassword, $dbHost){
+    $conn = new mysqli($databaseName, $dbUser, $dbPassword, $dbHost);
+    if($conn->connect_error)
+    {
+        echo "error try again \r\n";
+    }
+
+    echo "connected to database\r\n";
+    return $conn;
+}
+
